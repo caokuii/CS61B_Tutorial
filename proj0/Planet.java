1a7d8f20760ed75a34d7ece3301f13bf4b13c8fd
caokuii <3032264268@qq.com>
2023-04-02 17:57:17 +0800

    third commit proj0
    类中变量的权限，只在本类中使用的private就行

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -5,7 +5,7 @@ public class Planet {
     public double yyVel;// Its current velocity in the y direction
     public double mass;// Its mass质量
     public String imgFileName;// The name of the file that corresponds to the image that depicts the planet (for example, jupiter.gif)
-    public static double G = 6.67e-11;
+    private double G = 6.67e-11;
     //constructor
     public Planet(double xP, double yP, double xV, double yV, double m, String img){
         xxPos = xP;
